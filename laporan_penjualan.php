abb61b74af2cb0320e9c78222f56f1a7b2bc6189
arissetyawan1 <aris.setyawan354313@gmail.com>
2022-07-09 16:57:14 +0700

    fix : routing templates

--- laporan_penjualan.php
+++ laporan_penjualan.php
@@ -1,1 +1,6 @@
+<?php
+    require 'templates/header.php';
+    require 'templates/side_bar.php';
+?>
+
 <h2>Created by Aris ALporan Penjualan</h2>
--- laporan_permintaan.php
+++ laporan_permintaan.php
@@ -1,1 +1,7 @@
+<?php
+    require 'templates/header.php';
+    require 'templates/side_bar.php';
+?>
+
+
 <h2>Created by Aris Laporan Prediksi Permintaan</h2>
